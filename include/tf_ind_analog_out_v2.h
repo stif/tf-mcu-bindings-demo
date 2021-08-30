175a3c8017087c0a9667fe9e370724bca805d789
Stefan Reichhard <stefan@reichhard.info>
2021-08-30 15:25:52 +0200

    added Industrial Analog Out

---
+++ include/tf_ind_analog_out_v2.h
@@ -0,0 +1,20 @@
+#ifndef __TF_IND_ANALOG_OUT_V2_H__
+#define __TF_IND_ANALOG_OUT_V2_H__
+
+// This example is not self-contained.
+// It requres usage of the example driver specific to your platform.
+// See the HAL documentation.
+
+#include "hal_common.h"
+#include "bricklet_industrial_analog_out_v2.h"
+
+#define UID "XYZ" // Change XYZ to the UID of your Industrial Analog Out Bricklet 2.0
+
+extern void check(int rc, const char* msg);
+
+/*
+void analog_out_setup(TF_HalContext *hal);
+void analog_out_loop(TF_HalContext *hal);
+*/
+
+#endif // __TF_IND_ANALOG_OUT_V2_H__
---
+++ src/tf_ind_analog_out_v2.c
@@ -0,0 +1,17 @@
+#include "tf_ind_analog_out_v2.h"
+
+static TF_IndustrialAnalogOutV2 iao;
+
+void analog_out_setup(TF_HalContext *hal) {
+	// Create device object
+	check(tf_industrial_analog_out_v2_create(&iao, UID, hal), "create device object");
+
+	// Set output current to 4.5mA
+	check(tf_industrial_analog_out_v2_set_current(&iao, 4500), "call set_current");
+	check(tf_industrial_analog_out_v2_set_enabled(&iao, true), "call set_enabled");
+}
+
+void analog_out_loop(TF_HalContext *hal) {
+	// Poll for callbacks
+	tf_hal_callback_tick(hal, 0);
+}
