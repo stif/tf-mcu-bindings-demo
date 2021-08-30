7ebd344c4a07f0b3e0039d186fe97b788df6d412
Stefan Reichhard <stefan@reichhard.info>
2021-08-30 15:25:23 +0200

    added Industrial Analog In

---
+++ include/tf_ind_analog_in_v2.h
@@ -0,0 +1,21 @@
+#ifndef __TF_IND_ANALOG_IN_V2_H__
+#define __TF_IND_ANALOG_IN_V2_H__
+
+// This example is not self-contained.
+// It requres usage of the example driver specific to your platform.
+// See the HAL documentation.
+
+#include "hal_common.h"
+#include "bricklet_industrial_dual_analog_in_v2.h"
+
+#define UID "Sfw" // Change XYZ to the UID of your Industrial Dual Analog In Bricklet 2.0
+
+extern void check(int rc, const char* msg);
+/*
+void analog_in_setup(TF_HalContext *hal);
+void analog_in_loop(TF_HalContext *hal);
+*/
+static void voltage_handler(TF_IndustrialDualAnalogInV2 *device, uint8_t channel,
+                            int32_t voltage, void *user_data);
+
+#endif // __TF_IND_ANALOG_IN_V2_H__
---
+++ src/tf_ind_analog_in_v2.c
@@ -0,0 +1,31 @@
+#include "tf_ind_analog_in_v2.h"
+
+// Callback function for voltage callback
+static void voltage_handler(TF_IndustrialDualAnalogInV2 *device, uint8_t channel,
+                            int32_t voltage, void *user_data) {
+	(void)device; (void)user_data; // avoid unused parameter warning
+
+	tf_hal_printf("Channel: %I8u\n", channel);
+	tf_hal_printf("Voltage: %d 1/%d V\n", voltage, 1000.0);
+	tf_hal_printf("\n");
+}
+
+static TF_IndustrialDualAnalogInV2 idai;
+
+void analog_in_setup(TF_HalContext *hal) {
+	// Create device object
+	check(tf_industrial_dual_analog_in_v2_create(&idai, UID, hal), "create device object");
+
+	// Register voltage callback to function voltage_handler
+	tf_industrial_dual_analog_in_v2_register_voltage_callback(&idai,
+	                                                          voltage_handler,
+	                                                          NULL);
+
+	// Set period for voltage (channel 0) callback to 1s (1000ms) without a threshold
+	tf_industrial_dual_analog_in_v2_set_voltage_callback_configuration(&idai, 0, 1000, false, 'x', 0, 0);
+}
+
+void analog_in_loop(TF_HalContext *hal) {
+	// Poll for callbacks
+	tf_hal_callback_tick(hal, 0);
+}
